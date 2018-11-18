f8ed03424d371f6c25d8c1a6369e05c248c8a41e
Mauricio Marin <marinmauricio66@gmail.com>
2018-11-18 14:26:14 -0800

    add tracer bullet for client code base dummy component

---
+++ client/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import Dummy from './components/Dummy';
+import ReactDOM from 'react-dom';
+
+console.log('HELLO WORLD!!!!!');
+
+ReactDOM.render(<Dummy/>, document.getElementById('body'));
