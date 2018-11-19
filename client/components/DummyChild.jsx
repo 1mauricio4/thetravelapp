f3c41819763fa602525933672f01ff35ec6f64e1
Mauricio Marin <marinmauricio66@gmail.com>
2018-11-18 17:29:55 -0800

    Add back end tracer bullit with dummie data

--- client/components/DummyChild.jsx
+++ client/components/DummyChild.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import jQuery from 'jquery';
 
 export default class Dummy extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dataFromBackEnd: {}
+    };
+    jQuery.get('http://localhost:3000/').then((data) => {
+      this.setState({
+        dataFromBackEnd: data
+      })
+    })
+  }
   render () {
     return (
       <div className='container-child'>
         <h1>I'm the child</h1>
+        {this.state.dataFromBackEnd.countryName}
       </div>
     );
   }
