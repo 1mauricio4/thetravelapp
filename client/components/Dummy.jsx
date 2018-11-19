3f7838813536de9440a5c485bc4c1596d0830b1d
Mauricio Marin <marinmauricio66@gmail.com>
2018-11-19 15:35:24 -0800

    Add greeting component

--- client/components/Dummy.jsx
+++ client/components/Dummy.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import jQuery from 'jquery';
 import DummyChild from './DummyChild';
 
 export default class Dummy extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dataFromBackEnd: {}
+    };
+    jQuery.get('http://localhost:3000/').then((data) => {
+      this.setState({
+        dataFromBackEnd: data
+      });
+    });
+  }
   render () {
     return (
-      <div className='container'>
-        <h1> Getting Started</h1>
+      <div className='container greeting'>
+        <h1>{this.state.dataFromBackEnd.greeting}</h1>
         <DummyChild/>
       </div>
     );
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,14 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let country = {
-    countryName: 'USA',
-    population: '330,000',
-    capital: 'Washington D.C.',
-    president: 'Sunny D'
+  let data = {
+    greeting: 'Hello, Welcome to the USA!',
+    countryInfo: {
+      countryName: 'USA',
+      population: '330,000',
+      capital: 'Washington D.C.',
+      president: 'Sunny D'
+    }
   };
-
-  res.json(country);
+  res.json(data);
 });
 
 module.exports = router;
