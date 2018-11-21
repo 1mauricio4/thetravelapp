9b026afd96fb4cbfa98aaee34f22e88c35ec9a80
Mauricio Marin <marinmauricio66@gmail.com>
2018-11-20 16:31:53 -0800

    Add greeting component and styled-components

--- client/components/Dummy.jsx
+++ client/components/Dummy.jsx
@@ -1,25 +1,43 @@
 import React from 'react';
 import jQuery from 'jquery';
 import DummyChild from './DummyChild';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 export default class Dummy extends React.Component {
   constructor() {
     super();
     this.state = {
-      dataFromBackEnd: {}
+      dataFromBackEnd: {},
+      countryData: {}
     };
     jQuery.get('http://localhost:3000/').then((data) => {
       this.setState({
-        dataFromBackEnd: data
+        dataFromBackEnd: data,
+        countryData: data.countryInfo
       });
     });
   }
   render () {
     return (
-      <div className='container greeting'>
-        <h1>{this.state.dataFromBackEnd.greeting}</h1>
-        <DummyChild/>
-      </div>
+      <Wrapper>
+        <div className='container greeting'>
+          <Title>
+            <h1>{this.state.dataFromBackEnd.greeting}</h1>
+          </Title>
+          <DummyChild countryInfo={this.state.countryData.countryName}/>
+        </div>
+      </Wrapper>
     );
   }
 };
